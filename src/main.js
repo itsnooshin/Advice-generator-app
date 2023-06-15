74152e77a30f49b3e561589dad83221021e934c3
Nooshin bakhtiari <nooshindev@gmail.com>
2023-06-15 18:16:53 +0330

    add advice slip API to project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-console.log("hhh");
+'use strict';
 
-fetch("https://api.adviceslip.com/advice")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const btnmain = document.querySelector('.icon-button');
+const numberRandom = document.querySelector('.advice-number');
+const quoteRandom = document.querySelector('.lead-quote');
+
+btnmain.addEventListener('click', function () {
+  async function adviceGen() {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const data = await response.json();
+      const number = data.slip.id;
+      numberRandom.textContent = number;
+      const text = data.slip.advice;
+      quoteRandom.textContent = text;
+    } catch (err) {
+      throw new Error('API request failed');
+    }
+  }
+
+  adviceGen();
+});
